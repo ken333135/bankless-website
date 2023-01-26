8e947ab3d25145b29410f1d9da6e407abfb8857e
Yannis <yannis182@outlook.fr>
2023-01-26 11:18:01 +0100

    fix: Updated icon sizes on footer

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -129,12 +129,12 @@ class Footer extends React.Component {
                     href={_social.href}
                     target='_blank'
                     key={`social-${idx}`}
-                    margin={2}>
+                    margin={1}>
                     <Image
                       src={_social.img}
                       alt={_social.alt}
-                      width={20}
-                      height={20}
+                      width={30}
+                      height={30}
                     />
                   </Link>
                 )
